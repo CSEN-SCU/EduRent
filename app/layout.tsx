5830b0da496bdf51aa2365768518f295ef73e2f8
nkenny160 <nkenny@scu.edu>
2024-04-23 00:05:28 -0700

    fetch listing card is done

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,10 @@ export default async function RootLayout({
           <RegisterModal />
           <Navbar currentUser = {currentUser}/>
         </ClientOnly>
-        {children}</body>
+        <div className="pb-20 pt-28">
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
