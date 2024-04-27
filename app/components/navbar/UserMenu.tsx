497624e0b385a851a157555457565e02dc2538c6
anushaKankipati <anushakankipati1@gmail.com>
2024-04-27 16:02:29 -0700

    listing reservation component

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -41,7 +41,7 @@ const UserMenu: React.FC<UserMenuProps>  = ({
             onClick={onRent}
             className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer"
           >
-            Airbnb your home
+            EduRent your space
           </div>
           <div
             onClick={toggleOpen} // Corrected onClick handler
