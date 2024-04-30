5d7b50c96940197231015d0f024f60e22f149bad
Lucas Jablon <lucas.jablon@gmail.com>
2024-04-30 15:03:00 -0700

    Merged SearchBox

--- app/components/inputs/CountrySelect.tsx
+++ app/components/inputs/CountrySelect.tsx
@@ -25,31 +25,18 @@ const CountrySelect: React.FC<CountrySelectProps> = ({
         <div>
             <input
                 type="text"
-                placeholder="Enter Street Address"
+                placeholder="Enter Full Address"
                 value={value && value.label}
                 onChange={(event) => {
                     const newValue = { ...value, 
                         flag: '📍',
                         label: event.target.value,
                         latlng: [0,10000], // CHANGE // CHANGE// CHANGE// CHANGE
-                    };
-                    onChange(newValue as CountrySelectValue);
-                    console.log("value for CountrySelectValue:");
-                    console.log(newValue);
-                }}
-            />
-            <input
-                type="text"
-                placeholder="Enter City, State"
-                value={value && value.region}
-                onChange={(event) => {
-                    const newValue = { ...value, 
                         region: event.target.value,
-                        value: (value && value.label ? value.label + ', ' : '') + event.target.value}; 
-                    
+                        value: event.target.value
+                    };  
+                
                     onChange(newValue as CountrySelectValue);
-                    console.log("value for CountrySelectValue:");
-                    console.log(newValue);
                 }}
             />
         </div>
