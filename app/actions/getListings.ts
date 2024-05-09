8680a6e622f508764d61b79262ca70653a8690c7
anushaKankipati <anushakankipati1@gmail.com>
2024-05-09 11:15:20 -0700

    category filtering + readme

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -3,7 +3,13 @@ import prisma from "@/app/libs/prismadb"
 
 export interface IListingParams {
     userId?: string;
-    
+    guestCount?: number;
+    roomCount?: number;
+    bathroomCount?: number;
+    startDate?: string;
+    endDate?: string;
+    locationValue?: string;
+    category?: string;
   }
 
 export default async function getListings(
@@ -11,12 +17,42 @@ export default async function getListings(
 
 ) {
     try {
-        const { userId} = params;
+        const { 
+            userId,
+            roomCount,
+            guestCount,
+            bathroomCount,
+            locationValue,
+            startDate,
+            endDate,
+            category,
+
+        } = params;
 
         let query: any = {};
 
         if(userId){
             query.userId = userId;
+        }
+
+        if(category){
+            query.category = category;
+        }
+
+        if(roomCount){
+            query.roomCount = {
+                gte: +roomCount
+            }
+        }
+        if(guestCount){
+            query.guestCount = {
+                gte: +guestCount
+            }
+        }
+        if(bathroomCount){
+            query.bathroomCount = {
+                gte: +bathroomCount
+            }
         }
 
         const listings = await prisma.listing.findMany({
