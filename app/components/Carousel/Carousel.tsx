809731ebe985a7024340f95f2e9d5d5be85dbcce
Wesley Durbano <146608297+wesdurbano@users.noreply.github.com>
2024-05-28 15:36:07 -0700

    imports and dependancy for swipeable

---
+++ app/components/Carousel/Carousel.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useSwipeable } from "react-swipeable";
+import {
+  Wrapper,
+  CarouselContainer,
+  CarouselSlot,
+  SlideButton,
+  PREV,
+  NEXT
+} from "./Component";
---
+++ app/components/Carousel/Component.js
@@ -0,0 +1,4 @@
+import styled from "styled-components";
+
+export const NEXT = "NEXT";
+export const PREV = "PREV";
