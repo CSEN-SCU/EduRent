d60274b247e5c382660eb8bc744b380dc7f94dfe
Lucas Jablon <lucas.jablon@gmail.com>
2024-05-28 16:12:12 -0700

    formatting

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ const Home = async ({ searchParams }: HomeProps) => {
 
   const googleMapsApiKey = process.env.GOOGLE_MAPS_EMBED_KEY;
 
-  const center = { lat: 37.3489, lng: -121.9368 };//SCU coordinates
+  const center = { lat: 37.3489, lng: -121.9368 }; //SCU coordinates
   const zoom = 15;
 
   if (!googleMapsApiKey) {
@@ -36,24 +36,20 @@ const Home = async ({ searchParams }: HomeProps) => {
     <ClientOnly>
       <div className="flex h-screen justify-center md:flex-start">
         <div className="flex-initial lg:w-2/5 overflow-hidden">
-                <div className="sticky top-0 h-screen">
-                  <GoogleMap
-                    // apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}
-                    apiKey={googleMapsApiKey}
-                    zoom={zoom}
-                    center={center}
-                    listings={listings}
-                    currentUser={currentUser}
-                  // onIdle={onIdle}
-                  // onMarkerClick={onMarkerClick}
-                  // highlightedMarkerId={highlightedHotel?.hotelId}
-                  />
-            </div>
+          <div className="sticky top-0 h-screen">
+            <GoogleMap
+              apiKey={googleMapsApiKey}
+              zoom={zoom}
+              center={center}
+              listings={listings}
+              currentUser={currentUser}
+            />
           </div>
+        </div>
         <div className="flex-initial lg:w-3/5 overflow-y-auto h-full">
-          
           <Container>
-            <div className="
+            <div
+              className="
               pt-24
               pb-5
               grid
@@ -66,7 +62,8 @@ const Home = async ({ searchParams }: HomeProps) => {
               gap-8
               md:mt-20
               
-            ">
+            "
+            >
               {listings.map((listing) => {
                 return (
                   <ListingCard
@@ -74,7 +71,7 @@ const Home = async ({ searchParams }: HomeProps) => {
                     key={listing.id}
                     data={listing}
                   />
-                )
+                );
               })}
             </div>
           </Container>
