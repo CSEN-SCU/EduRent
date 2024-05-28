660e539d595d88fb24e6bf781e31d47bcca5314e
nkenny160 <nkenny@scu.edu>
2024-05-28 15:48:46 -0700

    scroll works kinda

--- app/components/listings/ListingHead.tsx
+++ app/components/listings/ListingHead.tsx
@@ -5,6 +5,10 @@ import Heading from "../Heading";
 import Image from "next/image";
 import { SafeUser } from "@/app/types";
 import HeartButton from "../inputs/HeartButton";
+import Carousel from "../Swiper/Carousel";
+import { Item, AppContainer, Code } from "../Swiper/components";
+import ReactDOM from "react-dom";
+import React from "react";
 
 interface IListingHeadProps {
   title: string;
@@ -47,10 +51,18 @@ const ListingHead: React.FC<IListingHeadProps> = ({
           sizes="100%"
           className="object-cover w-full"
         />
+
         <div className="absolute top-5 right-5">
           <HeartButton listingId={id} currentUser={currentUser} />
         </div>
       </div>
+      <Carousel title="Carousel">
+        <Item img="https://unsplash.it/475/205" />
+        <Item img="https://unsplash.it/476/205" />
+        <Item img="https://unsplash.it/477/205" />
+        <Item img="https://unsplash.it/478/205" />
+        <Item img="https://unsplash.it/479/205" />
+      </Carousel>
     </>
   );
 };
