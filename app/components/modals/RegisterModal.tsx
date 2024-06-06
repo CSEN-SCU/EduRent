c09823d5d04ac7c855eacd510c0a4e4082714b29
Lucas Jablon <lucas.jablon@gmail.com>
2024-06-05 23:22:13 -0700

    Modal hotfix done

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -1,138 +1,128 @@
-'use client';
+"use client";
 
-import axios from 'axios';
-import { AiFillGithub } from 'react-icons/ai';
-import { FcGoogle } from 'react-icons/fc';
-import { useCallback, useState } from 'react';
-import{
-    FieldValues,
-    SubmitHandler,
-    useForm
-} from 'react-hook-form';
+import axios from "axios";
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+import { useCallback, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
-import useRegisterModal from '@/app/hooks/useRegisterModal';
-import Modal from './Modal';
-import Heading from '../Heading';
-import Input from '../inputs/Input';
-import toast from 'react-hot-toast';
-import Button from '../Button';
-import { signIn } from 'next-auth/react';
-import useLoginModal from '@/app/hooks/useLoginModal';
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+import Modal from "./Modal";
+import Heading from "../Heading";
+import Input from "../inputs/Input";
+import toast from "react-hot-toast";
+import Button from "../Button";
+import { signIn } from "next-auth/react";
+import useLoginModal from "@/app/hooks/useLoginModal";
 
 const RegisterModal = () => {
-    const registerModal = useRegisterModal();
-    const loginModal = useLoginModal();
-    const [isLoading, setIsLoading] = useState(false);
+  const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
+  const [isLoading, setIsLoading] = useState(false);
 
-    const {
-        register,
-        handleSubmit,
-        formState:{
-            errors,
-        }
-    } = useForm<FieldValues>({
-        defaultValues: {
-            name: "",
-            email: "",
-            password: ""
-        }
-    });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
 
-    const onSubmit: SubmitHandler<FieldValues> = (data) =>{
-        setIsLoading(true);
-        axios.post('/api/register', data)
-            .then(() => {
-                registerModal.onClose();
-            })
-            .catch((error) =>{
-                toast.error('Something went wrong.')
-            })
-            .finally(()=>{
-                setIsLoading(false);
-            })
-    }
-
-    const toggle = useCallback(() => {
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+    axios
+      .post("/api/register", data)
+      .then(() => {
         registerModal.onClose();
-        loginModal.onOpen();
-    }, [loginModal, registerModal]);
+      })
+      .catch((error) => {
+        toast.error("Something went wrong.");
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
 
-    const bodyContent = (
-        <div className='flex flex-col gap-4'>
-            <Heading 
-                title='Welcome to EduRent'
-                subtitle="Create an account!"
-                
-            />
-            <Input 
-                id = "email"
-                label = "Email"
-                disabled = {isLoading}
-                register={register}
-                errors={errors}
-                required
-                />
-            <Input 
-                id = "name"
-                label = "Name"
-                disabled = {isLoading}
-                register={register}
-                errors={errors}
-                required
-                />
-            <Input 
-                id = "password"
-                type = "password"
-                label = "Password"
-                disabled = {isLoading}
-                register={register}
-                errors={errors}
-                required
-                />
-        </div>
-    )
+  const toggle = useCallback(() => {
+    registerModal.onClose();
+    loginModal.onOpen();
+  }, [loginModal, registerModal]);
 
-    const footerContent = (
-        <div className='flex flex-col gap-4 mt-3'>
-            <hr />
-            <Button 
-                outline
-                label="Continue with Google"
-                icon={FcGoogle}
-                onClick={() => signIn('google')}
-                />
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Heading title="Welcome to EduRent" subtitle="Create an account!" />
+      <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        type="password"
+        label="Password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  );
 
-            <div className='text-neutral-500 text-center mt-4 font-light'>
-                <div className='justify-center flex flex-row items-center gap-2'>
-                    <div>Already have an account?</div>
-                    <div onClick={toggle} 
-                        className='
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button
+        outline
+        label="Continue with Google"
+        icon={FcGoogle}
+        onClick={() => signIn("google")}
+      />
+
+      <div className="text-neutral-500 text-center mt-4 font-light">
+        <div className="justify-center flex flex-row items-center gap-2">
+          <div>Already have an account?</div>
+          <div
+            onClick={toggle}
+            className="
                             text-neutral-800 
                             cursor-pointer 
-                            hover:underline'
-                    >
-                        Log in
-                    </div>
-                </div>
-            </div>
-
+                            hover:underline"
+          >
+            Log in
+          </div>
         </div>
+      </div>
+    </div>
+  );
 
-    )
-
-    return(
-        <Modal
-            disabled = {isLoading}
-            isOpen = {registerModal.isOpen}
-            title = 'Register'
-            actionLabel='Continue'
-            onClose={registerModal.onClose}
-            onSubmit={handleSubmit(onSubmit)}
-            body={bodyContent}
-            footer={footerContent}
-
-        />
-    );
-}
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={registerModal.isOpen}
+      title="Register"
+      actionLabel="Continue"
+      onClose={registerModal.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
+      footer={footerContent}
+      canAdvance={true}
+    />
+  );
+};
 
 export default RegisterModal;
